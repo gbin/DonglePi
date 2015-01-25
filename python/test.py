e3f82e0b6e9fd62f7521835792b30895ab80495a
Guillaume Binet <gbin@google.com>
2015-01-25 08:01:42 -0800

    Fixed the python example

--- python/test.py
+++ python/test.py
@@ -14,30 +14,30 @@ def rotl(num, bits):
 LED_DEV = 0x70
 print("setup...")
 GPIO.setmode(GPIO.BOARD)
-#i2c = smbus.SMBus(0)
-#i2c.write_byte(LED_DEV, 0x21) # oscillator on
-#i2c.write_byte(LED_DEV, 0x81) # switch on with no blink
-#i2c.write_byte(LED_DEV, 0xEF) # brightness
-#print("write on display done.")
+i2c = smbus.SMBus(0)
+i2c.write_byte(LED_DEV, 0x21) # oscillator on
+i2c.write_byte(LED_DEV, 0x81) # switch on with no blink
+i2c.write_byte(LED_DEV, 0xEF) # brightness
+print("write on display done.")
 
-GPIO.setup(25, GPIO.OUT)
-GPIO.setup(22, GPIO.IN, GPIO.PUD_UP)
-#pattern = [0] * 16
-#for i in range(8):
-#  pattern[i*2] = 1 << i
-#  pattern[i*2+1] = 1 << (7-i)
+#GPIO.setup(25, GPIO.OUT)
+#GPIO.setup(22, GPIO.IN, GPIO.PUD_UP)
+pattern = [0] * 16
+for i in range(8):
+  pattern[i*2] = 1 << i
+  pattern[i*2+1] = 1 << (7-i)
 
-#pattern[6] = 24
-#pattern[7] = 24
-#pattern[8] = 24
-#pattern[9] = 24
+pattern[6] = 24
+pattern[7] = 24
+pattern[8] = 24
+pattern[9] = 24
 
 blink = 1
 while True:
-  GPIO.output(25, blink)
+  #GPIO.output(25, blink)
   print("INPUT = " + str(GPIO.input(22)))
   blink = 0 if blink == 1 else 1
-  #for i, value in enumerate(pattern):
-  #  i2c.write_byte_data(LED_DEV, i, value)
-  #pattern = [rotl(val,8) for val in pattern]
+  for i, value in enumerate(pattern):
+    i2c.write_byte_data(LED_DEV, i, value)
+  pattern = [rotl(val,8) for val in pattern]
   time.sleep(.9)
